60d24a2859a3713c3c9edf37e4b4bfb832a0a7fc
RosyPetrella <petrellamr@gmail.com>
2025-02-21 15:10:03 +0100

    complete snack 2

--- main.js
+++ main.js
@@ -46,6 +46,7 @@ console.log(biciLeggera);
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 const squadre = [
   { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
@@ -72,3 +73,17 @@ for (let i = 0; i < squadre.length; i++) {
 }
 
 console.log(squadre);
+
+const squadreFallisubiti = [];
+
+// Pusho i falli subiti  negli oggetti (?)
+for (let i = 0; i < squadre.length; i++) {
+  const thisSquadra = squadre[i];
+  squadreFallisubiti.push({
+    nome: thisSquadra.nome,
+    falliSubiti: thisSquadra.falliSubiti,
+  });
+}
+
+// Stampa il nuovo array con i nomi e i falli subiti
+console.log(squadreFallisubiti);
