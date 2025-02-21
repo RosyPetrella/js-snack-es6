2b9e961847d322f7e858ec1b541d14ff73fbf201
RosyPetrella <petrellamr@gmail.com>
2025-02-21 14:58:39 +0100

    edit

--- main.js
+++ main.js
@@ -46,3 +46,29 @@ console.log(biciLeggera);
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+
+const squadre = [
+  { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 },
+];
+
+// genero un numero casuale
+
+function generateRandomNumber() {
+  return Math.round(Math.random() * 10);
+}
+
+console.log(generateRandomNumber());
+
+// assegno un numero causale generato con la funzione precedente a Punti Fatti e Falli Subiti per ogni squadra
+
+for (let i = 0; i < squadre.length; i++) {
+  const thisSquadra = squadre[i];
+  thisSquadra.puntiFatti = generateRandomNumber();
+  thisSquadra.falliSubiti = generateRandomNumber();
+}
+
+console.log(squadre);
