c378a49cb6de49c3febe964a44ebf59065aea346
RosyPetrella <petrellamr@gmail.com>
2025-02-21 13:52:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// Snack 1
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+// nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const bici = [
+  {
+    nome: "bianca",
+    peso: 3,
+  },
+  {
+    nome: "rossa",
+    peso: 2,
+  },
+  {
+    nome: "verde",
+    peso: 5,
+  },
+  {
+    nome: "blu",
+    peso: 7,
+  },
+  {
+    nome: "nera",
+    peso: 4,
+  },
+];
+
+// Snack2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
